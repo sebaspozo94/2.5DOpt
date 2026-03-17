86028732702518ef4747f268bee0c20b8f55ef77
sebaspozo94 <sebaspozo94@gmail.com>
2026-03-17 16:35:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -41,6 +41,7 @@ with st.expander("🎯 App Objective", expanded=False):
     st.markdown("""
     **Objective:** Distribute a constant amount of material to maximize the stiffness of a 2.5D membrane structure 
     subjected to in-plane point loads.
+    * Constant Material = Vfrac(X*Y*t_{min})
     
     📄 [**Read the detailed code explanation and documentation here (PDF)**](https://github.com/sebaspozo94/2.5DOpt/blob/main/2.5DOpt.pdf)
     """)
